abfa0a943091c0578ed7172773d44c4da3007724
ceriyamariyam <ceriyamariyam97@gmail.com>
2019-01-24 16:04:31 +0530

    stud page move

--- app/src/main/java/com/example/admin/studentreg/StudActivity.java
+++ app/src/main/java/com/example/admin/studentreg/StudActivity.java
@@ -1,6 +1,7 @@
 package com.example.admin.studentreg;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,13 +12,21 @@ import android.widget.Toast;
 public class StudActivity extends AppCompatActivity {
 EditText ed1,ed2;
     Button b1,b2;
-    String s1,s2;
+    String s1,s2,checkusername;
     String sid="mzc";
     String sp="college";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stud);
+        SharedPreferences pref=getSharedPreferences("login",MODE_PRIVATE);
+        checkusername=pref .getString("sid",null);
+        if(checkusername!=null)
+        {
+            Intent i=new Intent(getApplicationContext(),loginActivity.class);
+            startActivity(i);
+        }
+
         ed1=(EditText)findViewById(R.id.sid);
         ed2=(EditText)findViewById(R.id.pswd);
         b1=(Button)findViewById(R.id.ln);
@@ -28,7 +37,13 @@ EditText ed1,ed2;
                 s1 = ed1.getText().toString();
                 s2 = ed2.getText().toString();
                 if ((s1.equals(sid)) && (s2.equals(sp))) {
+                    SharedPreferences.Editor editor=getSharedPreferences("login",MODE_PRIVATE).edit();
+                    editor.putString("sid",s1);
+                    editor.putString("pswd",s2);
+                    editor.apply();
                     Intent e = new Intent(getApplicationContext(), loginActivity.class);
+                   e.putExtra("sid",s1);
+                    e.putExtra("pswd",s2);
                     startActivity(e);
 
                     //        Toast.makeText(getApplicationContext(),s1,Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/admin/studentreg/loginActivity.java
+++ app/src/main/java/com/example/admin/studentreg/loginActivity.java
@@ -1,13 +1,45 @@
 package com.example.admin.studentreg;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class loginActivity extends AppCompatActivity {
-
+String usr,pass;
+    String getuser,getpass;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        b=(Button)findViewById(R.id.logout);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor=getSharedPreferences("login",MODE_PRIVATE).edit();
+                editor.clear();
+                editor.apply();
+                Intent i=new Intent(getApplicationContext(),StudActivity.class);
+                startActivity(i);
+            }
+        });
+        SharedPreferences sharedPreferences=getSharedPreferences("login",MODE_PRIVATE);
+        getuser=sharedPreferences.getString("sid",null);
+        getpass=sharedPreferences.getString("pswd",null);
+        Toast.makeText(getApplicationContext(),getuser,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),getpass,Toast.LENGTH_LONG).show();
+
+
+//        Intent e=getIntent();
+//        usr=e.getExtras().getString("sid");
+//        pass=e.getStringExtra("pswd");
+//        Toast.makeText(getApplicationContext(),usr,Toast.LENGTH_LONG).show();
+//        Toast.makeText(getApplicationContext(),pass,Toast.LENGTH_LONG).show();
+
     }
 }
